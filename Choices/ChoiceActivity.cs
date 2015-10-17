e6d3904f0962916b6794866cfd7eb77be528ccf0
Michael Vuong <michael_vuong@outlook.com>
2015-10-17 15:16:18 +0200

    CommitTest

---
+++ Choices/ChoiceActivity.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Choices
+{
+	[Activity (Label = "Activity2")]			
+	public class ChoiceActivity : Activity
+	{
+
+		TextView Choice1;
+		TextView Choice2;
+		public int Choice1count=0;
+		public int Choice2count=0;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			
+			base.OnCreate (bundle);
+
+			// change screen to Choice screen
+			SetContentView(Resource.Layout.Choice);
+			Choice1 = FindViewById<TextView>(Resource.Id.choice1);
+			Choice2 = FindViewById<TextView>(Resource.Id.choice2);
+
+			Choice1.Click += (sender, e) => {
+				Choice1count++;
+				Choice1.Text = Choice1count + " votes";
+				Choice2.Text = Choice2count + " votes";
+				};
+
+			Choice2.Click += (sender, e) => {
+				Choice2count++;
+				Choice1.Text = Choice1count + " votes";
+				Choice2.Text = Choice2count + " votes";
+				};
+		}
+	}
+}
+
---
+++ Choices/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Choices;
+
+namespace Choices
+{
+	[Activity (Label = "Choices", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		Button Menu1;
+		Button Menu2;
+		TextView Result1;
+		TextView Result2;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			//Find our controls
+			Menu1 = FindViewById<Button> (Resource.Id.Menu1);
+			Menu2 = FindViewById<Button> (Resource.Id.Menu2);
+
+			// wire up add task button handler
+			if(Menu1 != null) {
+				Menu1.Click += (sender, e) => {
+				StartActivity(typeof(ChoiceActivity));
+				};
+			}		
+		}
+
+		protected override void OnResume ()
+		{
+			base.OnResume ();
+		}
+	}
+}
+
+
